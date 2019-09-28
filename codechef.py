6bba87892b8fe4f57c18bfb15c3adcfdca1bdf1d
Yash Agrawal <yashagrawal300@gmail.com>
2019-09-28 21:41:18 +0530

    Update codechef.py

--- codechef.py
+++ codechef.py
@@ -2,13 +2,14 @@ t = int(input())
 for i in range(t):
     s = list(input())
     x = list(input())
-    s.sort()
     x.sort()
     s = ''.join(s)
-    x = ''.join(x)
     for j in range(len(s)):
         if s[j] in x:
+            x.remove(s[j])
             if j == len(s)-1:
-                print(x)
+                print(s, end= '')
+                print(''.join(x))
         else:
             print("Impossible")
+            break
