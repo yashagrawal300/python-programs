5b11b3aa916bdf49e152a1bac8dc086a419affd3
Yash Agrawal <yashagrawal300@gmail.com>
2019-10-19 15:59:02 +0530

    Create I_am_easy.py

---
+++ I_am_easy.py
@@ -0,0 +1,9 @@
+'''
+This is an easy problem. 
+
+All you need to do is to print the first 10 multiples of the number given in input.
+'''
+
+t = int(input())
+for i in range(1, 11, 1):
+    print(i*t)
