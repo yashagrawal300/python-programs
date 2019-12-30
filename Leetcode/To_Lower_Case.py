7adaa5d1ffee53fb704d2d7d8cf4db6cf4d08be6
Yash Agrawal <yashagrawal300@gmail.com>
2019-12-30 13:29:26 +0530

    Create To_Lower_Case.py

---
+++ Leetcode/To_Lower_Case.py
@@ -0,0 +1,8 @@
+'''Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.'''
+
+
+
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        return str.lower()
+        
